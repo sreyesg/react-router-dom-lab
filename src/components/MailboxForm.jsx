7f2c3b609134826a55ae76baa44f4f22a19084e1
sreyesg <santiagoreyes.g@gmail.com>
2024-11-03 20:49:23 -0800

    Create and start definition of MailboxForm component

---
+++ src/components/MailboxForm.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+const initialState = {boxholder: '', boxSize:''}
+
+export default function MailboxForm(props){
+    
+    const [formData, setFormData] = useState(initialState)
+    
+    const handlgeChange = (event) => {
+        event.preventDefault()
+        setFormData = {}
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="boxholder">Enter a Boxholder: </label>
+            <input 
+                type="text" 
+                name="boxholder" 
+                id="boxholder"
+                value={FormData.boxholder}
+                onChange={handlgeChange}
+                />
+            <label htmlFor="boxSize">Select a Box Size: </label>
+            <input 
+                type="text" 
+                name="boxSize" 
+                id="boxSize"
+                value={FormData.boxSize}
+                onChange={handlgeChange}
+                />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
