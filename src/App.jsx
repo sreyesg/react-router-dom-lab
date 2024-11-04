47e39d487c1a43d1ed4c0c8ad406389277794a75
sreyesg <santiagoreyes.g@gmail.com>
2024-11-03 16:21:19 -0800

    Add Routes to serve NavBar links, and test them with static text

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 // src/App.jsx
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar';
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<main><h1>Post Office</h1></main>}/>
+        <Route path='/mailboxes' element={<h2>All Mailboxes</h2>}/>
+        <Route path='/mailboxes/new' element={<h2>New Mailbox</h2>}/>
+      </Routes>
+    </>
+  )
 };
 
 export default App;
