d74d19d9b43ff7267fcdebe1ebbd245d18459cf9
deijahmonay <deijah@shack15.com>
2024-11-11 05:43:48 -0800

    update mailboxlist to include links for each mailbox + add link from react-router for navigation to mailbox details

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     <>
       <NavBar />
       <Routes>
-        <Route path='/' element={<main><h1>Post Office</h1></main>}/>
+        <Route path='/' element={<main><h1>Post Office</h1></main>}/> 
         <Route path='/mailboxes' element={<MailboxList mailboxes={mailboxes}/>}/>
         <Route path='/new-mailbox' element={<MailboxForm addMailbox={addMailbox}/>}/>
 
--- src/components/MailboxList.jsx
+++ src/components/MailboxList.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function MailboxList ( props ) {
+
   return (
     <>
     <h2>Mailbox List</h2>
     <ul>
       {props.mailboxes.map((currentMailbox) =>(
-        <li key={currentMailbox._id}>Mailbox {currentMailbox._id}</li>
+        <li key={currentMailbox._id}>
+        <Link to={`/mailboxes/${currentMailbox._id}`}>  
+        Mailbox {currentMailbox._id} </Link>
+          </li>
       ))}
     </ul>
     </>
